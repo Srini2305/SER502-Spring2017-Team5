d3a87cadb5778555ff1dbcb104991b4e0f417359
hariiyerasu1729 <hari.iyer@asu.edu>
2017-04-24 01:22:05 -0700

    Update ParseTree.py
    Integrated Symbol Table for compilation

--- ParseTree.py
+++ ParseTree.py
@@ -1,4 +1,5 @@
 import sys
+import re
 from antlr4 import *
 from antlr4.InputStream import InputStream
 from DevilsCodeLexer import DevilsCodeLexer
@@ -6,17 +7,41 @@ from DevilsCodeParser import DevilsCodeParser
 from DevilsCodeListener import DevilsCodeListener
 from DevilsCodeVisitor import DevilsCodeVisitor
 
-class DevilsCodelistener(DevilsCodeListener):
+class CustomDevilsCodeListener(DevilsCodeListener):
     def __init__(self):
         self.symbol_table = []
+        self.tokens = []
 
     def enterDec_stmt(self, ctx):
-        self.symbol_table.extend(str(ctx.getChild(1).getChild(0)))
+        self.symbol_table.append(str(ctx.getChild(1).getChild(0)))
 
-    def symbolTable_synthesis(self):
-        print("Symbol Table:")
-        print(self.symbol_table)
-	#To check the program against the symbol table in progress
+    def enterExpr(self, ctx):
+        self.tokens.append(str(ctx.getChild(0)))
+
+    def enterStmt(self, ctx):
+        if(str(ctx.getChild(0)) == "print "):
+            self.tokens.append(str(ctx.getChild(1)))
+        else:
+            self.tokens.append(str(ctx.getChild(0)))
+
+    def enterAssign_expr(self, ctx):
+        self.tokens.append(str(ctx.getChild(0)))
+
+    def enterMath_factor(self, ctx):
+        self.tokens.append(str(ctx.getChild(0)))
+
+    def enterCpr_term(self, ctx):
+        self.tokens.append(str(ctx.getChild(0)))
+
+    def symbolTableChecker(self):
+        literalNames = [ "main ", "(", ")", "{", "}",
+                     ";", "print ", "=", "+", "-", "*", "/",
+                     "while ", "if ", "else ", "true", "false", "int ", "bool " ]
+        self.tokens = [ x for x in self.tokens if not re.match('[\[\]]', x) and x not in literalNames]
+        self.tokens = set(self.tokens)
+        for id in self.tokens:
+            if id not in self.symbol_table:
+                print("ERROR: " + id + " not declared")
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
@@ -30,8 +55,8 @@ if __name__ == '__main__':
     tree = parser.program()
 
     lisp_tree_str = tree.toStringTree(recog=parser)
-    listener = DevilsCodelistener()
+    listener = CustomDevilsCodeListener()
     walker = ParseTreeWalker()
     walker.walk(listener, tree)
-    listener.symbolTable_synthesis()
+    listener.symbolTableChecker()
     print(lisp_tree_str)
