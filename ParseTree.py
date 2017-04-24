cb0ebf18f6f36f82fa77cdb6c7fc2f629c8c16e5
hariiyerasu1729 <hari.iyer@asu.edu>
2017-04-23 17:21:47 -0700

    Update ParseTree.py
    Added Symbol Table builder

--- ParseTree.py
+++ ParseTree.py
@@ -3,6 +3,20 @@ from antlr4 import *
 from antlr4.InputStream import InputStream
 from DevilsCodeLexer import DevilsCodeLexer
 from DevilsCodeParser import DevilsCodeParser
+from DevilsCodeListener import DevilsCodeListener
+from DevilsCodeVisitor import DevilsCodeVisitor
+
+class DevilsCodelistener(DevilsCodeListener):
+    def __init__(self):
+        self.symbol_table = []
+
+    def enterDec_stmt(self, ctx):
+        self.symbol_table.extend(str(ctx.getChild(1).getChild(0)))
+
+    def symbolTable_synthesis(self):
+        print("Symbol Table:")
+        print(self.symbol_table)
+	#To check the program against the symbol table in progress
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
@@ -16,7 +30,8 @@ if __name__ == '__main__':
     tree = parser.program()
 
     lisp_tree_str = tree.toStringTree(recog=parser)
-    f = open('Intermediate_Code', 'w')
-    f.write(''+lisp_tree_str+'\n')
-    f.close()
-    print('\nParse tree for the program: \n\n' + lisp_tree_str)
+    listener = DevilsCodelistener()
+    walker = ParseTreeWalker()
+    walker.walk(listener, tree)
+    listener.symbolTable_synthesis()
+    print(lisp_tree_str)
