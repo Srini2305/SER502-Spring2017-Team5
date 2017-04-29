e29003f3faba149772d054cfc3255cca4200a134
PankajSingh-ASU <PankajSingh.asu@gmail.com>
2017-04-29 16:10:46 -0700

    removed comments

--- src/compiler/DevilsCodeRuntime.java
+++ src/compiler/DevilsCodeRuntime.java
@@ -18,11 +18,18 @@ public class DevilsCodeRuntime {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		DevilsCodeRuntime coder=new DevilsCodeRuntime();
-		List<String> statements= coder.readFile();
-		for(String s: statements)
+		List<String> statements= null;
+		if(1>args.length)
+			statements=coder.readFile();
+		else
 		{
-			System.out.println(s);
+			//System.out.println(args[0]);
+			statements=coder.readFile(args[0]);
 		}
+//		for(String s: statements)
+//		{
+//			System.out.println(s);
+//		}
 		ReservedKeywords.init();
 		StatementsExecutor executor=new StatementsExecutor(statements);
 		List<String> output=executor.generateOutput();
@@ -38,6 +45,13 @@ public class DevilsCodeRuntime {
 		//write file name logic if required
 		StatementsReader reader=new StatementsReader();
 		return reader.readFile();
+	}
+	public List<String> readFile(String file)
+	{
+		//write file name logic if required
+		
+		StatementsReader reader=new StatementsReader();
+		return reader.readFile(file);
 	}
 
 }
