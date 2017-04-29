a3253acd6d3a1035b43a6f614797e3e33dcc6169
hariiyerasu1729 <hari.iyer@asu.edu>
2017-04-29 00:42:01 -0700

    Update DevilsCompiler.py

--- src/compiler/DevilsCompiler.py
+++ src/compiler/DevilsCompiler.py
@@ -1,25 +1,24 @@
 import sys
+import re
 from antlr4 import *
+from antlr4.InputStream import InputStream
 from DevilsCodeLexer import DevilsCodeLexer
 from DevilsCodeParser import DevilsCodeParser
 from DevilsCodeListener import DevilsCodeListener
 
-def main(argv):
-    if (len(argv) > 1):
-        input = FileStream(argv[1])
-    else:
-        input = StdinStream()
-
-    lexer = DevilsCodeLexer(input)
-    stream = CommonTokenStream(lexer)
-    parser = DevilsCodeParser(stream)
-    tree = parser.program()
-    printer = DevilsCodeListener()
-    walker = ParseTreeWalker()
-    walker.walk(printer, tree)
-
-    # Print the Tree
-    #print(tree.toStringTree(recog=parser))
- 
 if __name__ == '__main__':
-    main(sys.argv)
+    if len(sys.argv) > 1:
+        input_stream = FileStream(sys.argv[1])
+    else:
+        input_stream = InputStream(sys.stdin.readline())
+
+    lexer = DevilsCodeLexer(input_stream)
+    token_stream = CommonTokenStream(lexer)
+    parser = DevilsCodeParser(token_stream)
+    tree, isLexicalError = parser.program()
+
+    lisp_tree_str = tree.toStringTree(recog=parser)
+    listener = DevilsCodeListener()
+    walker = ParseTreeWalker()
+    walker.walk(listener, tree)
+    listener.symbolTableChecker(isLexicalError)
