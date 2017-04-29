8a8a9ac2d9043636ab2625e3935f28641318bfc3
hariiyerasu1729 <hari.iyer@asu.edu>
2017-04-28 17:16:20 -0700

    Eliminated custom Listener

--- src/ParseTree.py
+++ src/ParseTree.py
@@ -7,42 +7,6 @@ from DevilsCodeParser import DevilsCodeParser
 from DevilsCodeListener import DevilsCodeListener
 from DevilsCodeVisitor import DevilsCodeVisitor
 
-class CustomDevilsCodeListener(DevilsCodeListener):
-    def __init__(self):
-        self.symbol_table = []
-        self.tokens = []
-
-    def enterDec_stmt(self, ctx):
-        self.symbol_table.append(str(ctx.getChild(1).getChild(0)))
-
-    def enterExpr(self, ctx):
-        self.tokens.append(str(ctx.getChild(0)))
-
-    def enterStmt(self, ctx):
-        if(str(ctx.getChild(0)) == "print "):
-            self.tokens.append(str(ctx.getChild(1)))
-        else:
-            self.tokens.append(str(ctx.getChild(0)))
-
-    def enterAssign_expr(self, ctx):
-        self.tokens.append(str(ctx.getChild(0)))
-
-    def enterMath_factor(self, ctx):
-        self.tokens.append(str(ctx.getChild(0)))
-
-    def enterCpr_term(self, ctx):
-        self.tokens.append(str(ctx.getChild(0)))
-
-    def symbolTableChecker(self):
-        literalNames = [ "main ", "(", ")", "{", "}",
-                     ";", "print ", "=", "+", "-", "*", "/",
-                     "while ", "if ", "else ", "true", "false", "int ", "bool " ]
-        self.tokens = [ x for x in self.tokens if not re.match('[\[\]]', x) and x not in literalNames]
-        self.tokens = set(self.tokens)
-        for id in self.tokens:
-            if id not in self.symbol_table:
-                print("ERROR: " + id + " not declared")
-
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         input_stream = FileStream(sys.argv[1])
@@ -52,11 +16,10 @@ if __name__ == '__main__':
     lexer = DevilsCodeLexer(input_stream)
     token_stream = CommonTokenStream(lexer)
     parser = DevilsCodeParser(token_stream)
-    tree = parser.program()
+    tree, isLexicalError = parser.program()
 
     lisp_tree_str = tree.toStringTree(recog=parser)
-    listener = CustomDevilsCodeListener()
+    listener = DevilsCodeListener()
     walker = ParseTreeWalker()
     walker.walk(listener, tree)
-    listener.symbolTableChecker()
-    print(lisp_tree_str)
+    listener.symbolTableChecker(isLexicalError)
